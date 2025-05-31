f2daaf1d03efde2af178001094d734f2e4145f7b
a-chernenok <alexchernenok1@gmail.com>
2025-05-31 09:46:01 +0300

    .

--- app/src/main/java/com/example/test2healthapp2/MainActivity.kt
+++ app/src/main/java/com/example/test2healthapp2/MainActivity.kt
@@ -471,8 +471,8 @@ class MainActivity : ComponentActivity() {
 
                 val keypointNames = listOf(
                     "nose", "left_eye", "right_eye", "left_ear", "right_ear",
-                    "left_shoulder", "right_shoulder", "left_elbow", "right_elbow",
-                    "left_wrist", "right_wrist", "left_hip", "right_hip",
+                    "right_shoulder", "left_shoulder", "left_elbow", "right_elbow",
+                    "right_wrist", "left_wrist", "left_hip", "right_hip",
                     "left_knee", "right_knee", "left_ankle", "right_ankle"
                 )
 
